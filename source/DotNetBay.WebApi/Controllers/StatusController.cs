814d092398362b9857438aeb182b413f6130081c
marius.dubach@students.fhnw.ch <marius.dubach@students.fhnw.ch>
2015-05-01 13:51:38 +0200

    WepAPI not finished

---
+++ source/DotNetBay.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DotNetBay.WebApi.Controllers
+{
+    public class StatusController : ApiController
+    {
+      public IHttpActionResult GetStatus()
+      {
+        return this.Ok("Yes");
+      }
+    }
+}
