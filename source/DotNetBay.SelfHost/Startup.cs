bd57db70f1e64bc6a51eaed5a67a73d745d99295
marius.dubach@students.fhnw.ch <marius.dubach@students.fhnw.ch>
2015-05-20 22:47:48 +0200

    webapi now "working"

--- source/DotNetBay.SelfHost/Startup.cs
+++ source/DotNetBay.SelfHost/Startup.cs
@@ -13,11 +13,14 @@ namespace DotNetBay.SelfHost
         public void Configuration(IAppBuilder appBuilder)
         {
             HttpConfiguration config = new HttpConfiguration();
+
+            /*
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+             */
 
             config.MapHttpAttributeRoutes();
 
